d893a62fccb9b48b15af91f9e09554ce962d0c20
Anurag <anuragchandra1601@gmail.com>
2025-04-21 22:28:50 +0530

    Initial commit2

--- app.py
+++ app.py
@@ -9,17 +9,32 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-from flask import Flask, render_template
-from flask_cors import CORS
+# from flask import Flask, render_template
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+
+# # Enable CORS for all routes and origins
+# CORS(app)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=3000)
+
+
+
+
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
-# Enable CORS for all routes and origins
-CORS(app)
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
